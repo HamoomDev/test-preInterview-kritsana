752582d1d449e9ab15943806c323148a6dd2f958
new4452 <new4452@gmail.com>
2023-12-12 17:06:26 +0700

    add description

--- app/src/main/java/com/example/test_preinterview_kritsana/Subject1.kt
+++ app/src/main/java/com/example/test_preinterview_kritsana/Subject1.kt
@@ -11,8 +11,14 @@ class Subject1 : AppCompatActivity() {
     private lateinit var binding: ActivitySubject1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /***
+         * set bind data for activity
+         */
         binding = ActivitySubject1Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        /***
+         * set on click button to begin function
+         */
         binding.btnCal.setOnClickListener {
             calculate()
         }
@@ -20,16 +26,29 @@ class Subject1 : AppCompatActivity() {
 
     @SuppressLint("StringFormatInvalid")
     private fun calculate() = with(binding) {
+        /***
+         * get number from EditText
+         */
         val qtyA = edtA.text.toString().toDouble()
         val qtyB = edtB.text.toString().toDouble()
         val qtyC = edtC.text.toString().toDouble()
 
+        /***
+         * summery number
+         */
         val total = qtyA + qtyB + qtyC
 
+        /***
+         * calculate by data from edittext / summery data to total sale in type
+         * and X100 to percent last round data to Integer
+         */
         val saleOA = (((qtyA / total).toFloat()) * 100).roundToInt()
         val saleOB = (((qtyB / total).toFloat()) * 100).roundToInt()
         val saleOC = (((qtyC / total).toFloat()) * 100).roundToInt()
 
+        /***
+         * print data to screen by add data to resource string from parameter
+         */
         totalSale.text = resources.getString(R.string.total_sale, total.toInt())
         saleA.text = resources.getString(R.string.sale_of_a, saleOA)
         saleB.text = resources.getString(R.string.sale_of_b, saleOB)
