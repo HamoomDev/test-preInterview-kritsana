8c21be50b31721fa3b3eca6b9fc9388843e0350f
new4452 <new4452@gmail.com>
2023-12-13 14:30:01 +0700

    commit subject 4

--- app/src/main/java/com/example/test_preinterview_kritsana/Subject4.kt
+++ app/src/main/java/com/example/test_preinterview_kritsana/Subject4.kt
@@ -2,10 +2,23 @@ package com.example.test_preinterview_kritsana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.test_preinterview_kritsana.databinding.ActivitySubject4Binding
 
 class Subject4 : AppCompatActivity() {
+    private lateinit var binding: ActivitySubject4Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_subject4)
+        binding = ActivitySubject4Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            btn.setOnClickListener {
+                result.text = reverse(edt.text.toString())
+            }
+        }
+    }
+
+    private fun reverse(txt:String):String{
+        //คำสั่ง reverse ค่าใน string
+        return txt.reversed()
     }
 }
